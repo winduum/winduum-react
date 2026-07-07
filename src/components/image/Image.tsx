f730406fdde29466ef93101ef83ca5f5d64f6c1a
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-07-07 14:44:13 +0200

    feat: enhanced Image component with skeleton removal logic and updated documentation links

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,4 +1,4 @@
-import { createRef, HTMLProps } from "react"
+import { HTMLProps, useEffect, useRef } from "react"
 import { Slot } from "@radix-ui/react-slot"
 import classNames from "classnames"
 
@@ -8,8 +8,20 @@ interface Props extends HTMLProps<any> {
 }
 
 export default function Image(props: Props) {
-	const ref = createRef<HTMLElement>()
+	const ref = useRef<HTMLElement>(null)
 	const Comp = props.asChild ? Slot : props.as ?? "div"
+
+	useEffect(() => {
+		const element = ref.current?.querySelector("img, video, iframe")
+
+		if (!element) return
+
+		const removeSkeleton = () => ref.current?.classList.remove("before:skeleton")
+
+		if ("complete" in element && element.complete) removeSkeleton()
+		else if (element instanceof HTMLVideoElement) element.oncanplay = removeSkeleton
+		else element.addEventListener("load", removeSkeleton, { once: true })
+	}, [])
 
 	return (
 		<Comp {...props} className={classNames("x-image", props.className)} ref={ref}>
--- src/components/image/readme.md
+++ src/components/image/readme.md
@@ -1,5 +1,5 @@
-# [Image](https://winduum.dev/docs/ui/image.html)
+# [Image](https://winduum.dev/docs/components/image.html)
 
 ## Installation
 You can copy and paste the components in this directory to your own project.
-Follow the installation instruction at [winduum/winduum](https://github.com/winduum/winduum/tree/main/src/ui/image)
+Follow the installation instruction at [winduum/winduum](https://github.com/winduum/winduum/tree/main/src/components/image)
